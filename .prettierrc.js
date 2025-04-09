0b58928682d588c79a6634ff60f1ee6b1329efa8
RishabhVerma098 <vermarishabh0987@gmail.com>
2025-04-09 23:00:55 +0530

    build: squashing everything in main

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+export default {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  printWidth: 80,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  bracketSpacing: true,
+}
